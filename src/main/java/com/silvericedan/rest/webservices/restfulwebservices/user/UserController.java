a184fe0b7f61cf779be10b6e637decd7114c765c
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2022-08-18 15:46:57 -0300

    implemented DELETE method, also added spring-boot-starter-validation dependency

--- src/main/java/com/silvericedan/rest/webservices/restfulwebservices/user/UserController.java
+++ src/main/java/com/silvericedan/rest/webservices/restfulwebservices/user/UserController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +33,13 @@ public class UserController {
     if(user==null)
       throw new UserNotFoundException("id-"+id);
     return user;
+  }
+
+  @DeleteMapping("/users/{id}")
+  public void deleteUser(@PathVariable int id){
+    User user = service.deleteById(id);
+    if(user==null)
+      throw new UserNotFoundException("id-"+id);
   }
 
   //input - details of user
