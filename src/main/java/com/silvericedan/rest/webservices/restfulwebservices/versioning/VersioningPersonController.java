09089549fe3098a02ba71a76d3131c90659dfcbb
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2022-08-25 16:54:25 -0300

    Versioning with Request Param, Header and Content Negotiation

--- src/main/java/com/silvericedan/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/silvericedan/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningPersonController {
 
+  //Different options to do Versioning
+  //URI Versioning
   @GetMapping("/v1/person")
   public PersonV1 getFirstVersionOfPerson(){
     return new PersonV1("Bob Charlie");
@@ -14,5 +16,39 @@ public class VersioningPersonController {
   @GetMapping("/v2/person")
   public PersonV2 getSecondVersionOfPerson(){
     return new PersonV2(new Name("Bob","Charlie"));
+  }
+
+  //Request Parameter versioning - Amazon
+  @GetMapping(path ="/person", params = "version=1")
+  public PersonV1 getFirstVersionOfPersonRequestParameter(){
+    return new PersonV1("Buba Fisherman");
+  }
+
+
+  @GetMapping(path ="/person", params = "version=2")
+  public PersonV2 getSecondVersionOfPersonRequestParameter(){
+    return new PersonV2(new Name("Buba","Fisherman"));
+  }
+
+  //(Custom) headers versioning - Microsoft
+  @GetMapping(path ="/person/header", headers = "X-API-VERSION=1")
+  public PersonV1 getFirstVersionOfPersonRequestHeader(){
+    return new PersonV1("Helmeppo Headerman");
+  }
+
+  @GetMapping(path ="/person/header", headers = "X-API-VERSION=2")
+  public PersonV2 getSecondVersionOfPersonRequestHeader(){
+    return new PersonV2(new Name("Helmeppo","Headerman"));
+  }
+
+  //Media type versioning (aka "content negotiation" or "accept header") - Github
+  @GetMapping(path ="/person/accept", produces = "application/vnd.company.app-v1+json")
+  public PersonV1 getFirstVersionOfPersonAcceptHeader(){
+    return new PersonV1("Helmeppo Headerman");
+  }
+
+  @GetMapping(path ="/person/accept", produces = "application/vnd.company.app-v2+json")
+  public PersonV2 getSecondVersionOfPersonAcceptHeader(){
+    return new PersonV2(new Name("Helmeppo","Headerman"));
   }
 }
